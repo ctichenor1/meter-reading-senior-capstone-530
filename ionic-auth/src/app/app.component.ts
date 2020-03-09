0edf3435b35475820cde06b08bbde9e44d4af563
ctichenor1 <ctichenor1@murraystate.edu>
2020-03-09 17:04:54 -0500

    Upload code from Jonathan
    Implemented some api stuff from jonathan. I'm not entirely sure what it is, it should show you where the errors are.

--- ionic-auth/src/app/app.component.ts
+++ ionic-auth/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-
-import { Platform } from '@ionic/angular';
+import { ConnectionService } from '/services/connection-service'; 
+import {  Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
@@ -13,10 +13,14 @@ export class AppComponent {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    public connectionService: ConnectionService){
+
+    }
   ) {
     this.initializeApp();
   }
+t his.connectionService.apiValue.emit("localhost:58999");
 
   initializeApp() {
     this.platform.ready().then(() => {
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import Parse from 'parse';
-import { Platform } from '@ionic/angular';
+import { ConnectionService } from '../services/connection-services'; 
+import {  Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
@@ -10,26 +10,17 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
   styleUrls: ['app.component.scss']
 })
 export class AppComponent {
-  public appPages = [
-    {
-      title: 'Home',
-      url: '/home',
-      icon: 'home'
-    },
-    {
-      title: 'List',
-      url: '/list',
-      icon: 'list'
-    }
-  ];
-
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    public connectionService: ConnectionService){
+
+    }
   ) {
     this.initializeApp();
   }
+  this.connectionService.apiValue.emit("localhost:58999");
 
   initializeApp() {
     this.platform.ready().then(() => {
